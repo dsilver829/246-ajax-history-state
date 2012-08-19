b5435334fd7318d0969281252101526e0d48b061
David Silver <dsilver@candidatemetrics.com>
2012-08-19 10:03:38 -0700

    create seed file

--- store/db/seeds.rb
+++ store/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+for i in 1..10
+  Product.create!(name: "Product ##{i}", price: i+0.99, released_at: Time.now - i.days)
+end
